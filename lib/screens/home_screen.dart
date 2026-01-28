b4e1201af198328af538b259e4ed3fd4b3c741d5
EXAM <exam@DICTMCT-28.local>
2026-01-28 12:54:15 +0530

    SEU_IS_20_ICT_049: Update Home Screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -42,6 +42,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
