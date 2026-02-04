4212c990b0845bea4533c7daaa28448176f8c615
sithara sewwandi <bssitharasewwandi.00@gmail.com>
2026-02-04 12:00:38 +0530

    update widget file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:course_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Institute App());
+    await tester.pumpWidget(const InstituteApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
